01b657c3ddc6303a1535aeb87051aaddb04560a5
Amruth Pillai <im.amruth@gmail.com>
2019-01-24 02:23:51 +0530

    restart always policy

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const process = require('process');
 const redis = require('redis');
 
 const app = express();
 
-const client = redis.createClient();
+const client = redis.createClient({
+  host: 'redis',
+  port: 6379
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
   client.get('visits', (err, visits) => {
-    res.send(visits + 'people have visited this website.');
+    res.send(visits + ' people have visited this website.');
     client.set('visits', parseInt(visits) + 1)
   });
+});
+
+app.get('/exit', (req, res) => {
+  process.exit(0);
 });
 
 app.listen(8081, () => {
